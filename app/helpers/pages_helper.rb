c1eae63f7be2fe76a02fd3733fca978ec4c92af6
BlinkVonDoom <blynk.rankin@gmail.com>
2019-02-13 13:44:39 -0700

    added login views

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,9 @@
 module PagesHelper
+    def pages_path
+        if user_signed_in?
+          'pages/pages_path/signed_in'
+        else
+          'pages/pages_path/non_signed_in'
+        end
+      end
 end
