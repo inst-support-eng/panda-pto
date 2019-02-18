7a4588d6675d558c34b3ff56533bd30a840549b0
Connor Merchant <cmerchant@instructure.com>
2019-02-17 21:41:41 -0700

    Handles updating existing records

--- app/models/import_date_csv.rb
+++ app/models/import_date_csv.rb
@@ -1,12 +1,18 @@
 class ImportDateCsv < ApplicationRecord
-    require 'csv'
-    validates :date, :base_value, presence: true
+  require 'csv'
+  validates :date, :base_value, presence: true
     
-    def self.import(file)
-        CSV.foreach(file.path, {encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
-            # it may be nice to add some error handling here, ie importing already existing values
-            # but it may also be nice to easily update values w/ fresh imports
-            ImportDateCsv.create! row.to_hash
-        end
+  def self.import(file)
+    # !TECHDEBT this should be wrapped in some error handling
+    CSV.foreach(file.path, {encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
+      ImportDateCsv.create! row.to_hash
     end
+    # find all models and group them on keys which should be common
+    grouped = all.group_by{|x| [x.date]}
+    grouped.values.each do |duplicates|
+      most_recent = duplicates.pop # pop keeps last item , shift would keep first
+      # delete all duplicates
+      duplicates.each{|double| double.destroy} # duplicates can now be destroyed
+    end
+  end
 end
