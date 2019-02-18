24cc5ce17025a0e1079f5ac3c0e7d76ba7091a1d
Connor Merchant <cmerchant@instructure.com>
2019-02-17 20:07:41 -0700

    techincally working but it is possible to create duplicate records w/ multiple imports

--- app/controllers/import_date_csv_controller.rb
+++ app/controllers/import_date_csv_controller.rb
@@ -1,14 +1,14 @@
 class ImportDateCsvController < ApplicationController
   def index
-    @date_value = ImportDateCsv.all
+    #@date_value = ImportDateCsv.all
   end
 
   def import
-    if params([:file])
+    if params[:file]
       ImportDateCsv.import(params[:file])
-      redirect(root_url, "Date CSV imported")
+      redirect_to root_url, notice: "Date CSV imported"
     else  
-      redirect(root_url, "Please upload a valid CSV file")
+      redirect_to(root_url, "Please upload a valid CSV file")
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'import_date_csv/index'
   get 'import_date_csv/import'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :import_date_csv do
+  	collection { post :import}
+  end
   devise_for :users, :controllers => {:registrations => "registrations"}
   devise_scope :user do 
     get 'login', to: 'devise/sessions#new'
@@ -12,4 +14,5 @@ Rails.application.routes.draw do
   end
   
   root to: 'pages#index'
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
