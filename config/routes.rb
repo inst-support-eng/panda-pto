277a5e26f13275a701a5d40edf1dd4151521a982
BlinkVonDoom <blynk.rankin@gmail.com>
2019-02-18 11:56:28 -0700

    rake db:migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,11 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  get 'import_date_csv/index'
-  get 'import_date_csv/import'
-  resources :import_date_csv do
-  	collection { post :import}
-  end
-=======
   get 'date_values/index'
   get 'date_values/import'
   resources :date_values do
   	collection { post :import}
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
->>>>>>> a150e1b568eb01e58e150de67e386776b5cc0671
+  
   devise_for :users, :controllers => {:registrations => "registrations"}
   devise_scope :user do 
     get 'login', to: 'devise/sessions#new'
