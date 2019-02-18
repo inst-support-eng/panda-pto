f784e552bf32a9c05ff1d2ea31bf108743ffcf6c
Connor Merchant <cmerchant@instructure.com>
2019-02-17 23:10:46 -0700

    removes pre-refactor table

--- db/schema.rb
+++ db/schema.rb
@@ -22,13 +22,6 @@ ActiveRecord::Schema.define(version: 2019_02_18_053834) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "import_date_csvs", force: :cascade do |t|
-    t.date "date"
-    t.float "base_value"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
