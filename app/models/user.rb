e93472e09f352b9087bcee44bb7eadb3a5c5cd28
BlinkVonDoom <blynk.rankin@gmail.com>
2019-02-19 17:20:38 -0700

    calendar partial refactor

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-  has_many :request_date
-  
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_18_201029) do
+ActiveRecord::Schema.define(version: 2019_02_18_053834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,18 +18,6 @@ ActiveRecord::Schema.define(version: 2019_02_18_201029) do
   create_table "date_values", force: :cascade do |t|
     t.date "date"
     t.float "base_value"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "import_date_csvs", force: :cascade do |t|
-    t.date "date"
-    t.float "base_value"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "request_dates", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
