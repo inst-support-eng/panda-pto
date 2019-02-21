83925c5d90a7cc30f93928d588a77a906ea07217
BlinkVonDoom <blynk.rankin@gmail.com>
2019-02-21 16:14:37 -0700

    refactor calendar and added requests to render on sidebar

---
+++ app/helpers/pto_requests_helper.rb
@@ -0,0 +1,2 @@
+module PtoRequestsHelper
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :request_date
+  has_many :pto_requests
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
