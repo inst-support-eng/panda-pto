1d14f5bfdac34764d6927acf639da38d118e30c0
BlinkVonDoom <blynk.rankin@gmail.com>
2019-02-21 22:11:39 -0700

    fix sidebar display issue, add user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+def seed_users
+    User.create({ name: "test", email: "test@test.com", bank_value: "2000", password: 'password1', password_confirmation: 'password1'})
+end
+
+def seed_calendar
+    Calendar.create(date: "2019-10-01", base_value: 0, signed_up_total: 0, signed_up_agents: [], current_price: 0)
+end
+
+seed_users
