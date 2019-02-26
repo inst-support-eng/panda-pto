f64f07cd134f5461f3a3032361f74b6f0b27b400
Connor Merchant <cmerchant@instructure.com>
2019-02-26 00:33:11 -0700

    not really working, but *something*

--- app/models/date_value.rb
+++ app/models/date_value.rb
@@ -13,10 +13,19 @@ class DateValue < ApplicationRecord
       most_recent = duplicates.pop # pop keeps last item , shift would keep first
       # delete all duplicates
       duplicates.each{|double| double.destroy} # duplicates can now be destroyed
-
-      # push values to calendar table
-
-
     end
+      # push values to calendar table
+      # Calendar.create!(self.attribute.except("id", "created_at", "updated_at"))
+    # https://stackoverflow.com/questions/28089441/how-can-i-move-data-from-one-table-to-another-in-rails-migration
+    # working \o/ ~ish
+    DateValue.find_each do |x|
+      import = Calendar.first_or_initialize(
+        :date => x.date,
+        :base_value => x.base_value,
+      )
+      import.save!
+    end
+
   end
 end
+
