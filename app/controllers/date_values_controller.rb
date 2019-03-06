5ed8db03009a62958c7bc1af52e1e1b6d03a6e62
Connor Merchant <cmerchant@instructure.com>
2019-03-06 02:52:39 -0700

    working on import, need to stress test

--- app/controllers/date_values_controller.rb
+++ app/controllers/date_values_controller.rb
@@ -5,6 +5,12 @@ class DateValuesController < ApplicationController
   def import
     if params[:file]
       DateValue.import(params[:file])
+      DateValue.find_each do |x|
+        # !TECHDEBT doesn't update :base_value on import when re-importing existing records
+        import = Calendar.where(:date => x.date).first_or_create #.update_attribute(:base_value, x.base_value)
+        helpers.update_price(x.date, x.base_value)
+        #import.save!
+      end
       # likely want to build out more of an admin dashboard
       # so the redirect process will be different !TECHDEBT
       redirect_to root_url, notice: "Date CSV imported"
