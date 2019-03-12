81ec817e2dc5723846b622d298f6da128b39340e
BlinkVonDoom <blynk.rankin@gmail.com>
2019-03-12 15:06:21 -0600

    HumanityAPI cleanup

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -1,5 +1,4 @@
 module NavigationHelper
-
     def collapsible_links_partial_path
       if user_signed_in?
         'layouts/navigation/collapsible_elements/signed_in_links'
@@ -7,5 +6,4 @@ module NavigationHelper
         'layouts/navigation/collapsible_elements/non_signed_in_links'
       end
     end
-    
 end
--- lib/humanity_api.rb
+++ lib/humanity_api.rb
@@ -1,64 +1,66 @@
+# frozen_string_literal: true
+
 require 'httparty'
 
 class HumanityAPI
-    include HTTParty
-    base_uri 'https://www.humanity.com'
+  include HTTParty
+  base_uri 'https://www.humanity.com'
 
-    def self.create_request(request, user)
-        access_token = get_token
-        url = "#{base_uri}/api/v2/leaves?access_token=#{access_token}"
-        headers = { 'Content-Type' => 'application/x-www-form-urlencoded'}
-        options = {
-            "employee" => "#{user.humanity_user_id}",
-            "leavetype" => "304060", # vaction PTO request id in humanity 
-            "start_date" => "#{request.request_date}",
-            "end_date" => "#{request.request_date}",
-            "reason" => "#{request.reason}"
-        }
+  def self.create_request(request, user)
+    access_token = get_token
+    url = "#{base_uri}/api/v2/leaves?access_token=#{access_token}"
+    headers = { 'Content-Type' => 'application/x-www-form-urlencoded' }
+    options = {
+      'employee' => user.humanity_user_id.to_s,
+      'leavetype' => '304060', # vaction PTO request id in humanity
+      'start_date' => request.request_date.to_s,
+      'end_date' => request.request_date.to_s,
+      'reason' => request.reason.to_s
+    }
 
-        response = HumanityAPI.post(url, :headers => headers, :body => options)
-        response['data']['id']
-    end
+    response = HumanityAPI.post(url, headers: headers, body: options)
+    response['data']['id']
+  end
 
-    def self.approve_request(request_id)
-        access_token = get_token
-        url = "#{base_uri}/api/v2/leaves/#{request_id}?access_token=#{access_token}"
-        headers = { 'Content-Type' => 'application/x-www-form-urlencoded'}
-        options = { 'status' => 1 }
+  def self.approve_request(request_id)
+    access_token = get_token
+    url = "#{base_uri}/api/v2/leaves/#{request_id}?access_token=#{access_token}"
+    headers = { 'Content-Type' => 'application/x-www-form-urlencoded' }
+    options = { 'status' => 1 }
 
-        response = HumanityAPI.put(url, :headers => headers, :body => options)
-        puts response
-    end
+    response = HumanityAPI.put(url, headers: headers, body: options)
+    response
+  end
 
-    def self.delete_request(request_id)
-        access_token = get_token
-        url = "#{base_uri}/api/v2/leaves/#{request_id}?access_token=#{access_token}"
-        
-        response = HumanityAPI.delete(url)
-        puts response
-    end
+  def self.delete_request(request_id)
+    access_token = get_token
+    url = "#{base_uri}/api/v2/leaves/#{request_id}?access_token=#{access_token}"
 
-    def self.get_employees
-        access_token = get_token
-        url = "#{base_uri}/api/v2/employees?access_token=#{access_token}"
-        puts url
-        response = HumanityAPI.get(url)
-        response["data"]
-    end
+    response = HumanityAPI.delete(url)
+    response
+  end
 
-    def self.get_token
-        url = "#{base_uri}/oauth2/token.php"
-        headers = { 'Content-Type' => 'application/x-www-form-urlencoded'}
-        options = {
-            'grant_type' => 'password',
-            'client_secret' => ENV['HUMANITY_CLIENT_SECRET'],
-            'client_id' => ENV['HUMANITY_CLIENT_ID'],
-            'password' => ENV['HUMANITY_PASSWORD'],
-            'username' => ENV['HUMANITY_USERNAME']
-        }
+  def self.get_employees
+    access_token = get_token
+    url = "#{base_uri}/api/v2/employees?access_token=#{access_token}"
+    puts url
+    response = HumanityAPI.get(url)
+    response['data']
+  end
 
-        response = HumanityAPI.post(url, :headers => headers, :body => options)
-        access_token = response.parsed_response['access_token']
-        access_token
-    end
+  def self.get_token
+    url = "#{base_uri}/oauth2/token.php"
+    headers = { 'Content-Type' => 'application/x-www-form-urlencoded' }
+    options = {
+      'grant_type' => 'password',
+      'client_secret' => ENV['HUMANITY_CLIENT_SECRET'],
+      'client_id' => ENV['HUMANITY_CLIENT_ID'],
+      'password' => ENV['HUMANITY_PASSWORD'],
+      'username' => ENV['HUMANITY_USERNAME']
+    }
+
+    response = HumanityAPI.post(url, headers: headers, body: options)
+    access_token = response.parsed_response['access_token']
+    access_token
+  end
 end
