cbbb29f18475615bc97e3b9edf6794672707fab4
blinkvondom <blynk.rankin@gmail.com>
2019-03-18 17:06:50 -0600

    added tests

--- spec/controllers/pto_requests_controller_spec.rb
+++ spec/controllers/pto_requests_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe PtoRequestsController, type: :controller do
     before(:each) do 
         @user = FactoryBot.create(:user)
         @calendar = FactoryBot.create(:calendar)
-        sign_in :user, @user
+        sign_in(@user, scope: :user)
     end
     describe "POST #create" do 
         it "should allow user with enough credits to request a day" do
@@ -12,10 +12,13 @@ RSpec.describe PtoRequestsController, type: :controller do
             @user.reload 
             @calendar.reload
             expect(@user.bank_value).to eq(140)
-            expect(@calendar.signed_up_agents).to include(@user.name)
         end 
 
         it "should allow a user to request a day that they have not already requests off" do
+            post :create, params: {:user_id => @user.id, :reason => "butts", :request_date => 10.days.from_now, :cost => 10}
+            @user.reload 
+            @calendar.reload
+            expect(@calendar.signed_up_agents).to include(@user.name)
         end 
 
         it "should not allow a user to make a request if they do not have enough credits" do
