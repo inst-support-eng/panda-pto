c69841563f8b85687f293bc5525489c47dcba51a
Connor Merchant <cmerchant@instructure.com>
2019-03-18 22:56:18 -0600

    make off-today view work

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,8 @@
 class AdminController < ApplicationController
   def index
-    @offtoday = []
-    @offtoday = PtoRequest.where(:request_date => Date.today)
+    today = Calendar.find_by(:date => Date.today) 
+    @offtoday = today.signed_up_agents
+    # PtoRequest.where(:request_date => Date.today).each{ |x| @offtoday.push(x) }
     @agents = User.all
   end
 end
