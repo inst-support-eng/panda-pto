7855f7876a01a201739c609668d69954ff205830
Connor Merchant <cmerchant@instructure.com>
2019-03-20 16:52:12 -0600

    auto populate 90 bank_value on import

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -24,10 +24,10 @@ class Agent < ApplicationRecord
         generated_password = Devise.friendly_token.first(12)
         humanity_user_id = response.select { |res| res['email'] == x.email}
         
-        user = User.create!(:email => block.email, :password => generated_password, :name => x.name, :humanity_user_id => humanity_user_id[0]['id'])
+        user = User.create!(:email => block.email, :password => generated_password, :name => x.name, :bank_value => 90, :humanity_user_id => humanity_user_id[0]['id'])
         RegistrationMailer.with(user: user, password: generated_password).registration_email.deliver_now
       end
     end
 
   end
-end
+end 
