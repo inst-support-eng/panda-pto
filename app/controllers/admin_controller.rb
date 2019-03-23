7efa2791c6d5c2d807ef3f0fb9d1ee805e3affc6
Connor Merchant <cmerchant@instructure.com>
2019-03-22 20:09:43 -0600

    improved admin page error handling

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,12 +1,14 @@
 class AdminController < ApplicationController
   def index
-    if !Calendar.first.nil?
-      today = Calendar.find_by(:date => Date.today) 
-      @offtoday = today.signed_up_agents
-      # PtoRequest.where(:request_date => Date.today).each{ |x| @offtoday.push(x) }
+    today = Calendar.find_by(:date => Date.today) 
+    if today.nil? 
+      @offtoday = "You may exist outside of spacetime presently. Today does not exist inside the application, please contact the nearest adult." 
+    else 
+      @offtoday = today.signed_up_agents 
     end
-    if !User.first.nil?
-      @agents = User.all
-    end
+      
+  if !User.first.nil?
+    @agents = User.all
+  end
   end
 end
