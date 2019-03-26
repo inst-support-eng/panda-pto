dadba0ea8fff8790b990eef281a5b40a3a0dc11b
Connor Merchant <cmerchant@instructure.com>
2019-03-25 20:22:42 -0600

    pipe shift info to admin page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,9 +5,8 @@ class AdminController < ApplicationController
       @offtoday = "You may exist outside of spacetime presently. Today does not exist inside the application, please contact the nearest adult." 
     else
       @day = Date.today.wday
-      @scheduled = User.where('work_days @> ARRAY[?]::integer[]', Date.today.wday)
+      @scheduled = User.where('work_days @> ARRAY[?]::integer[]', Date.today.wday).order(:start_time)
       @offtoday = today.signed_up_agents
-      
     end
     User.any? ? @agents = User.all : @agents = []
   end
