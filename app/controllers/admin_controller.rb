bc0775f25ba9c4358da843365dd9303837901c77
Connor Merchant <cmerchant@instructure.com>
2019-03-27 19:12:03 -0600

    require login basic

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,5 @@
 class AdminController < ApplicationController
+  before_action :login_required
   def index
     today = Calendar.find_by(:date => Date.today) 
     if today.nil? 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+
+    def login_required
+        redirect_to login_path if current_user.blank?
+      end
 end
