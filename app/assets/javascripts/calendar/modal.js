c4fcac9629de02ab083d6224cd59ab47d8ca97c3
blinkvondom <blynk.rankin@gmail.com>
2019-03-29 11:04:25 -0600

    update day_of_modal language

--- app/assets/javascripts/calendar/modal.js
+++ app/assets/javascripts/calendar/modal.js
@@ -29,7 +29,11 @@ $(document).on('turbolinks:load', function () {
             currentDate.setMinutes(00)
             currentDate.setSeconds(00)
             // #!TECHDEBT, jump off a bridge.
-            if ((requestDate.toDateString() == currentDate.toDateString()||requestDate > currentDate) && requestDate.getMonth()-currentDate.getMonth() <= 9 && !isNaN(current_price.current_price)) {
+            if(requestDate.toDateString() == currentDate.toDateString()) {
+                $('.dayOfModal').modal('show')
+            }
+
+            if (requestDate > currentDate && requestDate.getMonth()-currentDate.getMonth() <= 9 && !isNaN(current_price.current_price)) {
                 $('.calendarModal').modal('show');
                 let closeButton = '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button >'
                 let displayCost = current_price.current_price * 8;
@@ -37,12 +41,8 @@ $(document).on('turbolinks:load', function () {
                     displayCost = current_price.current_price * 10;
                 }
 
-                if(requestDate.toDateString() == currentDate.toDateString()) {
-                    $('.modal-header').html(`New Request for ${e.target.id}, <div><h2>Requests made within 48 hours. Please also reach out to your MCO and Supervisor</h2></div>` + closeButton)
-                } else {
-                    $('.modal-header').html("New Request for " + e.target.id + closeButton)
-                }
-
+                $('.modal-header').html("New Request for " + e.target.id + closeButton)
+                
                 $('.request-total').html(`total: ${displayCost}`)
                 $('#pto_request_request_date').attr("value", e.target.id)
                 $('#pto_request_cost').attr("value", displayCost)
