5af0671c9371474c5f68b460138e06e7c0064945
Connor Merchant <cmerchant@instructure.com>
2019-03-28 19:58:30 -0600

    allow shift info to be omited in import

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -31,7 +31,7 @@ class Agent < ApplicationRecord
     update_info.team = x.team
     update_info.start_time = x.start_time
     update_info.end_time = x.end_time
-    update_info.work_days = x.work_days.split(",").map(&:to_i)
+    update_info.work_days = x.work_days.split(",").map(&:to_i) unless x.work_days.nil?
     update_info.position = x.position
     # check if a user who doesn't have a humanity account has one now
     update_info.humanity_user_id = HumanityAPI.set_humanity_id(x.email, response) if update_info.humanity_user_id == 0
