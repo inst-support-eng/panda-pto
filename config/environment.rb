05e5c6ebd5db92befabd7c2f2b1c9067b4800476
blinkvondom <blynk.rankin@gmail.com>
2019-04-04 15:03:29 -0600

    env updates

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,5 @@
 # Load the Rails application.
 require_relative 'application'
 
-# mailer host
-config.action_mailer.default_url_options = { :host => 'panda-pto-test.herokuapp.com' }
-
 # Initialize the Rails application.
 Rails.application.initialize!
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,6 +93,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # mailer setup
+  config.action_mailer.default_url_options = { :host => 'panda-pto-test.herokuapp.com' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true 
   ActionMailer::Base.delivery_method = :smtp
