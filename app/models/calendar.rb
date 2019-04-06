348ae83ea5b5383d6ac2dd2a718e271fa611d510
Connor Merchant <cmerchant@instructure.com>
2019-04-05 20:05:15 -0600

    refactor date value import, note: I left in all the guts to the old endpoint just-in-case

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,4 +1,16 @@
 class Calendar < ApplicationRecord
+    require 'csv'
+
+    def self.import(file)
+        CSV.foreach(file.path, {encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
+            day = find_by(:date => row[:date]) || new
+            day.base_value = row[:base_value]
+            day.date = row[:date]
+            day.save
+        end
+   
+    end
 
 end
+
 
