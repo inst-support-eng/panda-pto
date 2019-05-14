9a2eb5352fa2d22b3e807169a4f25dda6c8e7162
blinkvondom <blynk.rankin@gmail.com>
2019-05-14 10:48:20 -0600

    updated factories and calendar spec

--- spec/controllers/calendars_controller_spec.rb
+++ spec/controllers/calendars_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe CalendarsController, type: :controller do
     describe "GET #fetch_dates" do
         it "should render all calendar dates" do
             get :fetch_dates
-            expect(response).to have_http_status(:success)
+            expect(response).to have_http_status(302)
         end
     end
 end
