a585ab8ff37c04d65a8186425a0393ebe3e27441
Connor Merchant <cmerchant@instructure.com>
2019-08-16 15:50:07 -0600

    handle future request better when user is deleted

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -95,7 +95,7 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         #updating the user's password also kills active sessions
         @user.update(:is_deleted => 1, :password => SecureRandom.hex)
-        @user.pto_requests.where('request_date > ?', Date.today).destroy_all
+        delete_future_requesets
         redirect_to show_user_path(@user)
     end
 
@@ -119,4 +119,30 @@ class UsersController < ApplicationController
     def find_user
         @user = User.find(params[:user_id])
     end
+
+    def delete_future_requesets
+        user_future_requests = @user.pto_requests.where('request_date > ?', Date.today).to_a
+        user_future_requests.each do |r|
+            @user.bank_value += r.cost
+
+            if r.reason == 'no call / no show'
+                @user.no_call_show -= 1 unless @user.no_call_show.nil? || @user.no_call_show == 0
+            elsif r.reason == 'make up / sick day'
+                @user.make_up_days -= 1 unless @user.make_up_days.nil? || @user.make_up_days == 0
+            end
+
+            future_requests = PtoRequest.where(["request_date = ? and created_at > ?", r.request_date, r.created_at]).to_a
+            UpdatePrice.update_pto_requests(future_requests) if future_requests.count > 0
+
+            calendar = Calendar.find_by(:date => r.request_date)
+            calendar.signed_up_agents.delete(@user.name)
+            calendar.signed_up_total >= 1 ? calendar.signed_up_total -= 1 : calendar.signed_up_total = 0
+            calendar.save
+            UpdatePrice.update_calendar_item(calendar)
+            HumanityAPI.delete_request(r.humanity_request_id)
+            r.update(:is_deleted => 1)
+        end
+        @user.save
+    end
+
 end
