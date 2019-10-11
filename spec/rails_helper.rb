e2fc127d1963bf2dca0a22a477393276ce8eb2ab
blinkvondom <blynk.rankin@gmail.com>
2019-10-11 10:16:32 -0600

    pages and nav help rspecs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,9 +1,11 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
+require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+if Rails.env.production?
+  abort('The Rails environment is running in production mode!')
+end
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -42,19 +44,19 @@ RSpec.configure do |config|
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
   end
- 
+
   config.before(:each) do
     DatabaseCleaner.strategy = :transaction
   end
- 
-  config.before(:each, :js => true) do
+
+  config.before(:each, js: true) do
     DatabaseCleaner.strategy = :truncation
   end
- 
+
   config.before(:each) do
     DatabaseCleaner.start
   end
- 
+
   config.after(:each) do
     DatabaseCleaner.clean
   end
@@ -96,9 +98,10 @@ RSpec.configure do |config|
     config.default_max_wait_time = 10 # seconds
     config.default_driver        = :selenium
   end
-  Capybara.server = :puma 
+  Capybara.server = :puma
 
   RSpec.configure do |config|
-    config.include Devise::Test::ControllerHelpers, type: :controller
+		config.include Devise::Test::ControllerHelpers, type: :controller
+		config.include Devise::Test::ControllerHelpers, type: :helper
   end
 end
