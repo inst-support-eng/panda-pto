e0930df55c94795fda1c418a9379129a3ce47368
blinkvondom <blynk.rankin@gmail.com>
2019-10-11 10:16:32 -0600

    pages and nav help rspecs

--- spec/helpers/pages_helper_spec.rb
+++ spec/helpers/pages_helper_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe NavigationHelper, type: :helper do
-  describe 'signed in user' do
+RSpec.describe PagesHelper, type: :helper do
+	describe 'signed in user' do
     before(:each) do
       @user = FactoryBot.create(:user)
       sign_in(@user, scope: :user)
     end
 
     it "returns signed_in_links partial's path" do
-      expect(helper.collapsible_links_partial_path).to (
-         eq 'layouts/navigation/collapsible_elements/signed_in_links'
+      expect(helper.pages_path).to (
+         eq 'pages/pages_path/calendar'
        )
     end
   end
@@ -20,8 +20,8 @@ RSpec.describe NavigationHelper, type: :helper do
     end
 
     it "returns non_signed_in_links partial's path" do
-      expect(helper.collapsible_links_partial_path).to (
-        eq 'layouts/navigation/collapsible_elements/non_signed_in_links'
+      expect(helper.pages_path).to (
+        eq 'pages/pages_path/non_signed_in'
       )
     end
   end
