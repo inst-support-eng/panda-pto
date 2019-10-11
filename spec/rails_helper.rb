3359133196e1b277c4b34c4a58c009469038aed4
blinkvondom <blynk.rankin@gmail.com>
2019-10-11 10:19:55 -0600

    format update

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -101,7 +101,7 @@ RSpec.configure do |config|
   Capybara.server = :puma
 
   RSpec.configure do |config|
-		config.include Devise::Test::ControllerHelpers, type: :controller
-		config.include Devise::Test::ControllerHelpers, type: :helper
+    config.include Devise::Test::ControllerHelpers, type: :controller
+    config.include Devise::Test::ControllerHelpers, type: :helper
   end
 end
