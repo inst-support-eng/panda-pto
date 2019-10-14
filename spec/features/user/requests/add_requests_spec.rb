9db69f14d45a1bdd63d2644f74d816fa035fff19
blinkvondom <blynk.rankin@gmail.com>
2019-10-14 09:05:15 -0600

    selenium for adding requests

---
+++ spec/features/user/requests/add_requests_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.feature 'Add Requests', type: :feature do
+  before(:each) do
+    @user = FactoryBot.create(:user)
+    @calendar = FactoryBot.create(:calendar)
+  end
+
+  after(:each) do
+    User.destroy_all
+  end
+
+  scenario 'user is able to make a request from the calendar', js: true do
+    sign_in(@user)
+    visit(root_path)
+    find_by_id(@calendar.date.to_s).click
+
+    fill_in('pto_request_reason', with: 'selenium test')
+    click_on('create request')
+
+    expect('.day-off')
+    expect('a Delete')
+  end
+end
