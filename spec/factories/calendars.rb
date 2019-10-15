16679f9daf4ddd2a2575f4cad01f9342b7898396
blinkvondom <blynk.rankin@gmail.com>
2019-10-15 15:44:20 -0600

    formatting

--- spec/factories/calendars.rb
+++ spec/factories/calendars.rb
@@ -4,14 +4,14 @@ FactoryBot.define do
     base_value { 0 }
     signed_up_total { 0 }
     signed_up_agents { [] }
-		current_price { 0.5 }
-		
-		trait :calendar_tomorrow do
-			date { Date.tomorrow }
-		end
+    current_price { 0.5 }
 
-		trait :calendar_today do
-			date { Date.today }
-		end
+    trait :calendar_tomorrow do
+      date { Date.tomorrow }
+    end
+
+    trait :calendar_today do
+      date { Date.today }
+    end
   end
 end
