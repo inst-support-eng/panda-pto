1d7cc6f7727880b693e4e967c330e9ec31782abf
blinkvondom <blynk.rankin@gmail.com>
2019-10-15 14:42:07 -0600

    formatting / check mailers for requests

--- spec/controllers/calendar_l2s_controller_spec.rb
+++ spec/controllers/calendar_l2s_controller_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe CalendarL2sController, type: :controller do
-    describe "GET #fetch_dates" do
-        it "should render all calendar dates" do
-            get :fetch_dates
-            expect(response).to have_http_status(302)
-        end
+  describe 'GET #fetch_dates' do
+    it 'should render all calendar dates' do
+      get :fetch_dates
+      expect(response).to have_http_status(302)
     end
+  end
 end
