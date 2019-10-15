bd2bfc45756e45304054034c12ae11753f00cc65
blinkvondom <blynk.rankin@gmail.com>
2019-10-15 13:41:10 -0600

    specs for mailers

--- spec/mailers/coverage_mailer_spec.rb
+++ spec/mailers/coverage_mailer_spec.rb
@@ -1,5 +1,25 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe CoverageMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Coverage mailer' do
+    before(:each) do
+      @calendar = FactoryBot.create(:calendar, :calendar_today)
+    end
+
+    after(:each) do
+      Calendar.destroy_all
+    end
+
+    let(:mail) { CoverageMailer.off_today }
+
+    it 'renders the headers' do
+      expect(mail.subject).to include('Agents off today:')
+      expect(mail.to).to eq([ENV['MCO_EMAIL']])
+      expect(mail.from).to eq([ENV['GMAIL_USERNAME']])
+    end
+
+    it 'renders the body' do
+      expect(mail.body.encoded).to include('No agents are schedlued off today')
+    end
+  end
 end
