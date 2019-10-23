7e7e7aed096cd5dd91b5e46dd1c5bdbbace212fb
blinkvondom <blynk.rankin@gmail.com>
2019-10-23 15:30:26 -0600

    fixed specs to check for weekends and next months

--- spec/features/user/requests/add_requests_spec.rb
+++ spec/features/user/requests/add_requests_spec.rb
@@ -12,9 +12,14 @@ RSpec.feature 'Add Requests', type: :feature do
 
   scenario 'user is able to make a request from the calendar', js: true do
     sign_in(@user)
-    visit(root_path)
-    find_by_id(@calendar.date.to_s).click
+		visit(root_path)
 
+		if @calendar.date.month != Date.today.month
+			find_by_id('next').click
+		end
+
+		find_by_id(@calendar.date.to_s).click
+		
     fill_in('pto_request_reason', with: 'selenium test')
     click_on('create request')
 
