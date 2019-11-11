284cddecd179396761f8b54a87bd6140ebcddf9d
blinkvondom <blynk.rankin@gmail.com>
2019-11-11 11:56:41 -0700

    api call for sharpen sms

---
+++ lib/sharpen_api.rb
@@ -0,0 +1,38 @@
+require 'httparty'
+
+##
+# Sharpen APIs for sending sms text messages
+class SharpenAPI
+  include HTTParty
+  base_uri 'https://api.fathomvoice.com'
+
+  ##
+  # sends SMS message to array of numbers from Sharpens API
+  # params:
+  # +message+:: string of message to be used in SMS
+  # +user_array+:: array of phone numbers to be sent
+  def self.send_sms(message, user_array)
+    url = "#{base_uri}/V2/messages/send/"
+
+    headers = {
+      'content-type' => 'application/json'
+    }
+
+    options = {
+      'cKey1' => ENV['CKEY1'],
+      'cKey2' => ENV['CKEY2'],
+      'toNumbers' => user_array,
+      'fromNumber' => ENV['SHARPEN_NUM'],
+      'text' => message
+    }
+
+    response = SharpenAPI.post(url, headers: headers, body: options.to_json)
+    response = JSON.parse(response)
+
+    unless response['status'] == 'Complete'
+      raise 'SMS message did not send correctly'
+    end
+
+    response['description']
+  end
+end
