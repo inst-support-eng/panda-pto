772597d0e49d569307513f35ef9ea6eb66740153
blinkvondom <blynk.rankin@gmail.com>
2019-11-11 12:01:44 -0700

    add spec to check for multiple numbers

--- spec/lib/sharpen_api_spec.rb
+++ spec/lib/sharpen_api_spec.rb
@@ -6,4 +6,11 @@ describe 'Sharpen API' do
     response = SharpenAPI.send_sms('rspec', ENV['TEST_PH_NUM'])
     expect(response).to eq('A total of 1 messages have been sent.')
   end
+
+  it 'should send multiple sms and give a Complete response' do
+    response = SharpenAPI.send_sms(
+      'rspec', [ENV['TEST_PH_NUM'], ENV['TEST_PH_NUM']]
+    )
+    expect(response).to eq('A total of 2 messages have been sent.')
+  end
 end
